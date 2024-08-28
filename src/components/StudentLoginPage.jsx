8b2d6e025a3d450111a4f0c2fbea314d7e593264
Siddharth Boorgu <siddharth.boorgu@xdp.co.in>
2024-08-28 11:58:21 +0530

    gradgrove frontend updated final

--- src/components/StudentLoginPage.jsx
+++ src/components/StudentLoginPage.jsx
@@ -15,6 +15,8 @@ const StudentLogin = () => {
   const navigate = useNavigate();
   const [displayEmailError, setDisplayEmailError] = useState("");
 
+  const [displayError, setDisplayError] = useState("");
+
   const handleEmail = (e) => {
     const email = e.target.value;
     setEmail(email);
@@ -49,11 +51,11 @@ const StudentLogin = () => {
       if (data) {
         navigate("/studentDashBoard");
       } else {
-        setDisplayEmailError("Enter the Registered Email");
+        setDisplayEmailError("Enter the Correct Password");
         console.log(displayEmailError);
       }
     } else {
-      setDisplayEmailError("Enter the Registered Email and Correct Password ");
+      setDisplayError("Enter the Registered Email and Correct Password ");
       console.log(displayEmailError);
     }
 
@@ -96,55 +98,115 @@ const StudentLogin = () => {
           <img src="src/images/logo.svg" alt="Logo" />
         </div>
       </header>
-
-      <section className="content">
-        <h1 className="TitleStudentLogin">
-          <b>Student Login</b>
-        </h1>
-
-        <form className="form space-y-6" onSubmit={studentLoginDetailsPost}>
-          <div className="form-group flex items-center space-x-4">
-            <label htmlFor="email" className="w-1/4">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              className="form-input w-full p-2 border border-gray-300 rounded textColor"
-              required
-              value={Email}
-              onChange={handleEmail}
-            />
-            {emailError && <div className="text-red-500">{emailError}</div>}
-            {displayEmailError && (
-              <div className="text-red-500">{displayEmailError}</div>
-            )}
-          </div>
-
-          <div className="form-group flex items-center space-x-4">
-            <label htmlFor="password" className="w-1/4">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              className="form-input w-full p-2 border border-gray-300 rounded textColor"
-              required
-              value={Password}
-              onChange={handlePassword}
-            />
-            {passwordError && (
-              <div className="text-red-500">{passwordError}</div>
-            )}
-          </div>
-
-          <button
-            type="submit"
-            className="submit-button bg-blue-600 text-white px-6 py-2 rounded"
+      <section style={{ backgroundColor: "rgb(68,86,115)" }}>
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <div
+            className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
+            style={{ marginTop: "-30%", transform: "scale(1.3)" }} // Doubles the size
           >
-            Login
-          </button>
-        </form>
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                Sign in to your account
+              </h1>
+              <form
+                className="space-y-4 md:space-y-6"
+                action="#"
+                onSubmit={studentLoginDetailsPost}
+              >
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Your email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="name@company.com"
+                    required=""
+                    value={Email}
+                    onChange={handleEmail}
+                  />
+                  {emailError && (
+                    <div className="text-red-500">{emailError}</div>
+                  )}
+                  {displayEmailError && (
+                    <div className="text-red-500">{displayEmailError}</div>
+                  )}
+                </div>
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required=""
+                    value={Password}
+                    onChange={handlePassword}
+                  />
+                  {passwordError && (
+                    <div className="text-red-500">{passwordError}</div>
+                  )}
+                </div>
+                <div className="flex items-center justify-between">
+                  <div className="flex items-start">
+                    <div className="flex items-center h-5">
+                      <input
+                        id="remember"
+                        aria-describedby="remember"
+                        type="checkbox"
+                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                        required=""
+                      />
+                    </div>
+                    <div className="ml-3 text-sm">
+                      <label
+                        htmlFor="remember"
+                        className="text-gray-500 dark:text-gray-300"
+                      >
+                        Remember me
+                      </label>
+                    </div>
+                  </div>
+                  <a
+                    href="#"
+                    className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  >
+                    Forgot password?
+                  </a>
+                </div>
+                <button
+                  type="submit"
+                  className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  Sign in
+                </button>
+                {displayError && (
+                  <div className="text-red-500">{displayError}</div>
+                )}
+                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                  Don’t have an account yet?{" "}
+                  <a
+                    href="#"
+                    className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  >
+                    Sign up
+                  </a>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
       </section>
 
       <footer className="footer mt-8">
